ed8b94fec84ea508edc4b481074e5c335221fcc0
Taylorpadams <taylorpadams@gmail.com>
2015-06-15 17:43:04 -0700

    added google-api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,8 @@ class UsersController < ApplicationController
     end
   end
 
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :developer unless Rails.env.production?
 	provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"] 
+	provider :google, ENV['GOOGLE_API_CLIENT_ID'], ENV['GOOGLE_API_CLIENT_SECRET']
 	# {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
