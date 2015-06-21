d35d0e7094b811f6b384794688482711253bd65a
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-21 16:59:08 -0700

    friendly date format

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,10 @@ class Appointment < ActiveRecord::Base
 	belongs_to :user, :class_name => "User"
   	belongs_to :provider, :class_name => "User"
   	validates_uniqueness_of :event_id, :on => :create
+
+
+  	def date_published
+      created_at.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+      start_time.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+    end
 end
