44522a1854e69d5fe93cfb78bc08d63c5e1ce601
Mike McCready <michaeljmccready@gmail.com>
2015-06-21 01:03:41 -0700

    tidied the code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,22 @@
 class UsersController < ApplicationController
-  #before_action :set_user, only: [ :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-      @user = User.find(params[:id])
-      @appointments = @user.appointments
+    @user = User.find(params[:id])
+    @appointments = @user.appointments
   end
 
-  # GET /users/new
+
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
-  # def create
-  #   @user = User.new(user_params)
-
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   def create
     @user = User.new(user_params)
@@ -50,8 +29,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -64,8 +41,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+
   def destroy
     @user.destroy
     respond_to do |format|
--- app/models/providership.rb
+++ app/models/providership.rb
@@ -1,5 +1,4 @@
 class Providership < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :provider, :class_name => "User"
-
 end
