cbc3932078e24fbef9794cf0d5d5fdaa01ef113f
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-23 18:19:52 -0700

    change callback routes in google developper

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
 		access_type: 'offline',
 	    scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/calendar',
-	    redirect_uri: 'https://apptyapp.herokuapp.com/auth/google_oauth2/callback'
+	    redirect_uri: 'http://apptyapp.herokuapp.com/auth/google_oauth2/callback'
 	}
 
 
