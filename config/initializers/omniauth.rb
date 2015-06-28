ad4b112b0957845ccb23571478f1fb025c2a1001
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-28 15:30:44 -0700

    provider developper removed in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	
-	provider :developer unless Rails.env.production?
+	# provider :developer unless Rails.env.production?
 
 	# provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"], scope: "user:email"
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
