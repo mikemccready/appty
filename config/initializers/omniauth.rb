56eeed627f78dcc59e4e145d9dcfc7a9301a6c45
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-29 01:56:18 -0700

    jess fixed it

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	
-	#provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"], scope: "user:email"
+	provider :developer unless Rails.env.production?
+
+	# provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"], scope: "user:email"
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
 		access_type: 'offline',
-		# :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}},
-
 	    scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/calendar',
-
-	    redirect_uri: 'https://localhost3000.info/google_oauth2/callback/'
-	    #redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback'
-
+	    #redirect_uri: 'https://apptyapp.herokuapp.com/auth/google_oauth2/callback'
+	    redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback'
 	}
 
-end 
+
+end
 
 
