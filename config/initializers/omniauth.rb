305e478ae02e5f84fb14dc9bf23ac7df5e6236af
Mike McCready <michaeljmccready@gmail.com>
2015-06-28 21:31:25 -0700

    heroku work please

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	#provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"], scope: "user:email"
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
 		access_type: 'offline',
-		:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}},
+		# :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}},
 
 	    scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/calendar',
 	    redirect_uri: 'https://apptytyme.herokuapp.com/auth/google_oauth2/callback'
