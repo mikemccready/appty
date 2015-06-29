6e5580220044008608bbdd50b6f13b4fc7db9fe9
Mike McCready <michaeljmccready@gmail.com>
2015-06-28 23:41:56 -0700

    fixes missing end omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	
-	 provider :developer unless Rails.env.production?
-
 	#provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"], scope: "user:email"
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
 		access_type: 'offline',
@@ -14,7 +12,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
 	}
 
-
-
+end 
 
 
