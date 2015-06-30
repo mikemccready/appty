c24c367d455661643185e60ad99d25b861524b50
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-30 16:31:29 -0700

    changed routes for heroku trial

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
         access_type: 'offline',
         scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/calendar',
         #redirect_uri: 'https://apptyapp.herokuapp.com/auth/google_oauth2/callback'
-        redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback'
+        redirect_uri: 'https://apptyapp.herokuapp.com/auth/google_oauth2/callback'
     }
 
 
